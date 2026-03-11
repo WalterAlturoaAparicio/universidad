7b2424e42af40e3e16a12a598c8fcb786f073349
TSebv <113436744+TSebv@users.noreply.github.com>
2026-03-11 17:18:36 +0000

    cambios

--- universidad/urls.py
+++ universidad/urls.py
@@ -5,8 +5,13 @@ app_name = "universidad"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("programas/", views.program_list, name="programs"),
-    path("estudiantes/", views.student_list, name="students"),
-    path("cursos/", views.course_list, name="courses"),
-]
 
+    path("programas/", views.program_list, name="programs"),
+    path("programas/<int:id>/", views.program_detail, name="program_detail"),
+
+    path("cursos/", views.course_list, name="courses"),
+    path("cursos/<int:id>/", views.course_detail, name="course_detail"),
+
+    path("estudiantes/", views.student_list, name="students"),
+    path("estudiantes/<int:id>/", views.student_detail, name="student_detail"),
+]
--- universidad/views.py
+++ universidad/views.py
@@ -1,14 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Program, Course, Student
 
-# Create your views here.
+
 def home(request):
-    return render(request, "universidad/home.html", {})
+    return render(request, "universidad/home.html")
 
-def student_list(request):
-    return render(request, "universidad/home.html", {})
+
+# LISTAS
 
 def program_list(request):
-    return render(request, "universidad/home.html", {})
+    programs = Program.objects.all()
+    return render(request, "universidad/program_list.html", {
+        "programs": programs
+    })
+
 
 def course_list(request):
-    return render(request, "universidad/home.html", {})
+    courses = Course.objects.select_related("program").all()
+    return render(request, "universidad/course_list.html", {
+        "courses": courses
+    })
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, "universidad/student_list.html", {
+        "students": students
+    })
+
+
+# DETALLES
+
+def program_detail(request, id):
+    program = get_object_or_404(Program, id=id)
+    return render(request, "universidad/program_detail.html", {
+        "program": program
+    })
+
+
+def course_detail(request, id):
+    course = get_object_or_404(Course, id=id)
+    return render(request, "universidad/course_detail.html", {
+        "course": course
+    })
+
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+    return render(request, "universidad/student_detail.html", {
+        "student": student
+    })
